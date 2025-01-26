ebeea79b058d722b4f0a9f79c3cb5a03f266f387
Ankit Minhas <mankitminhas@gmail.com>
2025-01-26 05:54:29 +0530

    code clean

--- src/wrapper/AnimationWrapper.tsx
+++ src/wrapper/AnimationWrapper.tsx
@@ -12,26 +12,28 @@ type Props = {
 const AnimationWrapper = ({ children }: Props) => {
   const progress = useDrawerProgress();
   const { width } = useWindowDimensions();
-  console.log(progress.value);
 
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [
-      { rotate: `${interpolate(progress.value, [0, 1], [0, -10])}deg` },
-      {
-        translateX: interpolate(
-          progress.value,
-          [0, 1],
-          [0, width - 300],
-          'clamp',
-        ),
-      },
-      {
-        translateY: interpolate(progress.value, [0, 1], [0, 100], 'clamp'),
-      },
-    ],
-    borderRadius: interpolate(progress.value, [0, 1], [0, 30], 'clamp'),
-    overflow: 'hidden',
-  }));
+  const animatedStyle = useAnimatedStyle(() => {
+    const rotation = `${interpolate(progress.value, [0, 1], [0, -10])}deg`;
+    const translateX = interpolate(
+      progress.value,
+      [0, 1],
+      [0, width - 300],
+      'clamp',
+    );
+    const translateY = interpolate(progress.value, [0, 1], [0, 100], 'clamp');
+    const borderRadius = interpolate(progress.value, [0, 1], [0, 30], 'clamp');
+
+    return {
+      transform: [
+        { rotate: rotation },
+        { translateX: translateX },
+        { translateY: translateY },
+      ],
+      borderRadius: borderRadius,
+      overflow: 'hidden',
+    };
+  });
 
   return (
     <Animated.View style={[styles.container, animatedStyle]}>
